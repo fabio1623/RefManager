62b66aebe39c16c6f1ba4df795f4d725785e05f2
fabio1623 <sarmentopedrofabio@gmail.com>
2015-10-30 18:27:52 +0100

    Expertises

---
+++ resources/views/references/create/english/modals/expertises_modal.blade.php
@@ -0,0 +1,26 @@
+<!-- Modal -->
+<div class="modal fade" id="expertises_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Add sub expertises</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" role="form" method="POST" action="{{ action('ExpertiseController@store') }}">
+          <?php echo csrf_field(); ?>
+          <div class="form-group">
+            <label class="col-sm-3 col-sm-offset-1 control-label">Expertise name</label>
+            <div class="col-sm-4">
+              <input type="text" class="form-control" id="expertise_name">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
