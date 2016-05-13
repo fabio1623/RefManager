e437db2257647509ed141f7eb86088aecd9af82e
fabio1623 <contact@fabio-sp.fr>
2016-05-13 12:02:36 +0200

    Lato Font integration

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -9,7 +9,7 @@
 		<title>References Manager</title>
 
 		<!-- Fonts -->
-	    <link href="http://intra.int-net.biz/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+	    <!-- <link href="http://intra.int-net.biz/css/font-awesome.min.css" rel='stylesheet' type='text/css'> -->
 	    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
 		<!-- Bootstrap -->
